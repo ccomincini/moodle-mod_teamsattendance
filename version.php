33e800918a7cf56d856dd1aea300842e2eaad992
Carlo Comincini <carlo@comincini.it>
2025-07-19 12:40:40 +0200

    Update version to 2025071903 v1.1.23 - fix IT language strings

--- version.php
+++ version.php
@@ -24,12 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025071902; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2025071903; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022112800; // Requires this Moodle version.
 $plugin->supported = [39, 40];   // Available as of Moodle 3.9.0 or later.
 $plugin->component = 'mod_teamsattendance'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE; // This version's maturity level.
-$plugin->release = 'v1.1.22'; // Human-readable version name.
+$plugin->release = 'v1.1.23'; // Human-readable version name.
 
 // Register the module's icon
 $plugin->icon = 'pix/icon.svg';
